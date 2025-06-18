4a877d88bb91b5929789f6cfb36a988b59c6735c
Devanshu2k <54180663+Devanshu2k@users.noreply.github.com>
2025-06-18 21:29:21 +0530

    Time: 3 ms (99.92%), Space: 44.9 MB (94.58%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,28 +1,39 @@
 class Solution {
+
+    List<Integer>[] convertToAdjList(int[][] prerequisites,int n){
+        List<Integer>[] adjList = new ArrayList[n];
+        for(int i=0;i<n;i++){
+            adjList[i] = new ArrayList<>();
+        }
+        for(int[] dep:prerequisites){
+            adjList[dep[0]].add(dep[1]);
+        }
+
+        return adjList;
+    }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer>[] adjList =  convertToAdjList(prerequisites,numCourses);
         boolean[] visited = new boolean[numCourses];
         boolean[] pathVisited = new boolean[numCourses];
         for(int i=0;i<numCourses;i++){
-            if(dfs(i,prerequisites,visited,pathVisited))
+            if(!visited[i] && dfs(i,adjList,visited,pathVisited))
                 return false;
         }
 
         return true;
     }
 
-    boolean dfs(int curr,int[][] prerequisites,boolean[] visited,boolean[] pathVisited){
+    boolean dfs(int curr,List<Integer>[] adjList,boolean[] visited,boolean[] pathVisited){
         visited[curr] = true;
         pathVisited[curr] = true;
 
-        for(int[] dep : prerequisites){
-            if(dep[0]==curr){
-                if(!visited[dep[1]]){
-                    if(dfs(dep[1],prerequisites,visited,pathVisited))
+        for(int dep : adjList[curr]){
+                if(!visited[dep]){
+                    if(dfs(dep,adjList,visited,pathVisited))
                         return true;
                 }
-                else if(pathVisited[dep[1]])
+                else if(pathVisited[dep])
                     return true;
-            }
         }
 
         pathVisited[curr]=false;
