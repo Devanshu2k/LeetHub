dc6f3319338f118a6edf81ec7ee247f1d96d3060
Devanshu2k <54180663+Devanshu2k@users.noreply.github.com>
2025-06-24 23:13:45 +0530

    Time: 2 ms (69.82%), Space: 45.6 MB (29.16%) - LeetHub

---
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+
+        int[][] minHealthArray = new int[m+1][n+1];
+
+        for(int i=0;i<m;i++)
+            minHealthArray[i][n]=Integer.MAX_VALUE;
+        
+        for(int i=0;i<n;i++)
+            minHealthArray[m][i]=Integer.MAX_VALUE;
+
+        minHealthArray[m-1][n]=1;
+        minHealthArray[m][n-1]=1;
+
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                minHealthArray[i][j] = Math.min(minHealthArray[i+1][j],minHealthArray[i][j+1])-dungeon[i][j];
+                if(minHealthArray[i][j]<=0)
+                    minHealthArray[i][j]=1;
+            }
+        }
+        return minHealthArray[0][0];
+    }
+
+}
