2bc19cc721b3d3a6432b18a7e17200d5c0748b86
Devanshu2k <54180663+Devanshu2k@users.noreply.github.com>
2025-07-16 08:56:48 +0530

    Time: 3 ms (91.36%), Space: 43 MB (88.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,15 @@
 class Solution {
-    public boolean isAnagram(String str1, String str2) {
-        if(str1.length()!=str2.length())
-            return false;
-        int n = str1.length();
-        char[] s1 = str1.toCharArray();
-        char[] s2 = str2.toCharArray();
-        
-        Arrays.sort(s1);
-        Arrays.sort(s2);
+    public boolean isAnagram(String s, String t) {
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
 
-        
-        for(int i=0;i<n;i++)
-            if(s1[i]!=s2[i])
-                return false;
+        for(int i=0; i<s.length(); i++) {
+            freq1[s.charAt(i)-'a']++;
+        }
+        for(int i=0; i<t.length(); i++) {
+            freq2[t.charAt(i)-'a']++;
+        }
 
-        return true;
+        return Arrays.equals(freq1, freq2);
     }
 }
