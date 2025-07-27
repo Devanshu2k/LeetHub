d4779e6db25d049a2f80ba771defdf86fa73bae1
Devanshu Shende <54180663+Devanshu2k@users.noreply.github.com>
2025-07-27 10:54:20 +0530

    Time: 82 ms (57.32%), Space: 58.1 MB (38.11%) - LeetHub

---
+++ 0883-car-fleet/0883-car-fleet.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = speed.length;
+        int count = 0;
+
+        int[][] combined = new int[n][2];
+
+        for(int i=0;i<n;i++){
+            combined[i][0] = position[i];
+            combined[i][1] = speed[i];
+        }
+
+        Arrays.sort(combined, (a, b) -> Integer.compare(b[0], a[0]));
+
+        Stack<Float> st = new Stack<>();
+
+        for(int i=0;i<n;i++){
+            float temp = (target-combined[i][0])/(float)combined[i][1];
+            if(!st.isEmpty() && st.peek()>=temp){
+                continue;
+            }
+
+            st.push(temp);
+        }
+
+        while(!st.isEmpty()){
+            count++;
+            st.pop();
+        }
+
+        return count;
+    }
+}
