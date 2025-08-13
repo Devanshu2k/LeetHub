7677ef245b0bb8a8e6c52e1584b6d261093ba3bf
Devanshu Shende <54180663+Devanshu2k@users.noreply.github.com>
2025-08-13 11:32:16 +0530

    Time: 2 ms (99.8%), Space: 49.5 MB (62.6%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -7,8 +7,8 @@ class Solution {
         int count=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(!visited[i][j] && grid[i][j]=='1'){
-                    bfs(grid,new int[]{i,j},visited);
+                if(grid[i][j]=='1'){
+                    dfs(grid,i,j);
                     count++;
                 }
             }
@@ -41,5 +41,17 @@ class Solution {
                 }
             }
         }
+    }
+
+    void dfs(char[][] grid,int i,int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]=='0')  
+            return;
+        
+        grid[i][j] = '0';
+
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);        
     }
 }
