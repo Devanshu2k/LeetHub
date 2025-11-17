e1bb99b66101ef7529238c39ae6c1a309e47851e
Devanshu Shende <54180663+Devanshu2k@users.noreply.github.com>
2025-11-17 15:58:48 +0530

    Time: 0 ms (100%), Space: 44.2 MB (11.84%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,36 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null)
+        if(list1==null)
             return list2;
-        if (list2 == null)
+        if(list2==null)
             return list1;
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
 
-        ListNode curr;
-        if (list1.val > list2.val) {
-            curr = list2;
-            list2 = list2.next; // Advance the smaller list
-        } else {
-            curr = list1;
-            list1 = list1.next; // Advance the smaller list
+        while(list1!=null & list2!=null){
+            if(list1.val>list2.val){
+                curr.next = list2;
+                list2 = list2.next;
+            } else{
+                curr.next = list1;
+                list1 = list1.next;
+            }
+
+            curr = curr.next;
         }
 
-        // Call the recursive helper
-        curr.next = sort(list1, list2);
-        return curr;
-    }
-
-    ListNode sort(ListNode list1, ListNode list2) {
-        if (list1 == null)
-            return list2;
-        if (list2 == null)
-            return list1;
-
-        if (list1.val > list2.val) {
-            list2.next = sort(list1, list2.next); // Move to the next node in list2
-            return list2;
-        } else {
-            list1.next = sort(list1.next, list2); // Move to the next node in list1
-            return list1;
-        }
+        if(list1==null)
+            curr.next = list2;
+        if(list2==null)
+            curr.next = list1;
+        
+        return dummy.next;
     }
 }
