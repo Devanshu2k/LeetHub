41ac3e48a8086ac82e7af93b5c4c036d2044853f
Devanshu Shende <54180663+Devanshu2k@users.noreply.github.com>
2025-11-19 17:33:02 +0530

    Time: 1 ms (58.99%), Space: 44.1 MB (13.95%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res= new ArrayList<>();
-
-        subset(nums,res,new ArrayList<>(),0);
+        List<List<Integer>> res = new ArrayList<>();
+        helper(res,new ArrayList<>(),nums,0);
         return res;
     }
 
-    void subset(int[] nums, List<List<Integer>> res,List<Integer> li,int i){
-        if(i==nums.length){
+    void helper(List<List<Integer>> res,List<Integer> li,int[] nums,int curr){
+        if(curr==nums.length){
             res.add(new ArrayList<>(li));
             return;
         }
 
-        li.add(nums[i]);
-        subset(nums,res,li,i+1);
-        li.remove(Integer.valueOf(nums[i]));
-        subset(nums,res,li,i+1);
+        li.add(nums[curr]);
+        helper(res,li,nums,curr+1);
+
+        li.remove(li.size()-1);
+        helper(res,li,nums,curr+1);
     }
 }
