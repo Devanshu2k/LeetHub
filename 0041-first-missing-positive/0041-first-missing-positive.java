5270819e1f9d1156973f118d16ea2389ab3918f4
Devanshu Shende <54180663+Devanshu2k@users.noreply.github.com>
2025-12-04 11:12:28 +0530

    Time: 1 ms (99.99%), Space: 70.7 MB (48.45%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: Place each value in its correct index
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int correct = nums[i] - 1;
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+        }
+
+        // Step 2: Find first mismatch
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1)
+                return i + 1;
+        }
+
+        return n + 1;
+    }
+}
