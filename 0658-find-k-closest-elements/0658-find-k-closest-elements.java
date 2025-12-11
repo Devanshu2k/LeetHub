c52db169e6934ad6a63d03e1749042592ba979f3
Devanshu Shende <54180663+Devanshu2k@users.noreply.github.com>
2025-12-11 17:02:48 +0530

    Time: 15 ms (33.81%), Space: 47.8 MB (39.28%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,22 +1,41 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> {
-            int da = Math.abs(x-a);
-            int db = Math.abs(x-b);
-            if(da!=db)
-                return db-da;
-            return b-a;
-            });
-        for(int i:arr){
-            pq.offer(i);
-            if(pq.size()>k)
-                pq.poll();
+        List<Integer> li = new ArrayList<>();
+        if(k==0)
+            return li;
+
+        int minIdx = 0;
+        int n = arr.length;
+        for(int i=1;i<n;i++){
+                if(Math.abs(arr[i]-x)<Math.abs(arr[minIdx]-x)){
+                    minIdx = i;
+                }
         }
 
-        List<Integer> li = new ArrayList<>();
-        while(!pq.isEmpty())
-            li.add(pq.poll());
-        Collections.sort(li);
+        li.add(arr[minIdx]);
+        k--;
+
+        int left = minIdx-1;
+        int right = minIdx+1;
+        while(k>0){
+            int dleft = Integer.MAX_VALUE;
+            int dright = Integer.MAX_VALUE;
+            
+            if(left>=0)
+                dleft = Math.abs(arr[left]-x);;
+            if(right<n)
+                dright = Math.abs(arr[right]-x);
+            if(dleft<=dright){
+                li.add(0,arr[left]);
+                    left--;
+                    k--;
+            } else{
+                 li.add(arr[right]);
+                    right++;
+                    k--;
+            }
+        }
+
         return li;
     }
 }
