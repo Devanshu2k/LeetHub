de3dfce041fd704508109f33f33afafa200f9cfd
Devanshu Shende <54180663+Devanshu2k@users.noreply.github.com>
2026-01-01 17:55:04 +0530

    Time: 10 ms (99.18%), Space: 98.3 MB (6.98%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-       Arrays.sort(nums);
-
-       for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==nums[i+1])
+        Set<Integer> set = new HashSet<>();
+        for(int i:nums){
+            if(!set.add(i))
                 return true;
-       }
-
-       return false;
+        }
+        return false;
     }
 }
