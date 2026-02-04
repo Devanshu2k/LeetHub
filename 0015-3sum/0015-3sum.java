9ddc6b2006ced26000b8e40ec58d78929681765d
Devanshu Shende <54180663+Devanshu2k@users.noreply.github.com>
2026-02-04 17:57:35 +0530

    Time: 33 ms (70.78%), Space: 59.1 MB (71.59%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,31 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] arr) {
-        int n = arr.length;
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(arr);
-
-        for (int i = 0; i < n; i++) {
-            //remove duplicates:
-            if (i != 0 && arr[i] == arr[i - 1]) continue;
-
-            //moving 2 pointers:
-            int j = i + 1;
-            int k = n - 1;
-            while (j < k) {
-                int sum = arr[i] + arr[j] + arr[k];
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
-                } else {
-                    List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
-                    ans.add(temp);
-                    j++;
-                    k--;
-                    //skip the duplicates:
-                    while (j < k && arr[j] == arr[j - 1]) j++;
-                    while (j < k && arr[k] == arr[k + 1]) k--;
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+            
+            int l = i+1;
+            int r = n-1;
+            while(l<r){
+                int sum = nums[i]+nums[l]+nums[r];
+                if(sum==0){
+                    res.add(List.of(nums[i],nums[l],nums[r]));
+                    while(l<r && nums[l]==nums[l+1]) l++;
+                    while(l<r && nums[r]==nums[r-1]) r--;
+                    l++;
+                    r--;
                 }
-            }
+                else if(sum>0)
+                    r--;
+                else
+                    l++;
+            }            
         }
 
-        return ans;
-    }
+            
+        return res;
+    } 
 }
